5ad6fdde9408e3a482e4873fd9ccd9a39fb84c11
Ilya Smut <107916229+ilya-smut@users.noreply.github.com>
2023-02-13 15:13:18 +0000

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!\n"
+  return 0;
+}
+
+/*
+To compile and run:
+$ g++ helloworld.cpp -o hello
+$ ./hello
+
+OUTPUT:
+Hello World!
+*/
