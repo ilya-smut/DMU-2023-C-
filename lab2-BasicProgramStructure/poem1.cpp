f669c31f49164e6f1ade16aadfa1a119c3d01e42
Ilya Smut <p2688910@dmu.ac.uk>
2023-02-14 11:47:08 +0000

    14/02/2023

---
+++ lab2-BasicProgramStructure/poem1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Ensanguining the sky How heavily it dies Into the west away\nPast touch and sight and sound No longer to be found\nHow hopeless underground Falls the remorseless day\n";
+}
