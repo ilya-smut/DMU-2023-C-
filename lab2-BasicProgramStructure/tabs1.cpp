d0ce3885e9b31216b45c7683b1b373fef863dc17
Ilya Smut <p2688910@dmu.ac.uk>
2023-02-14 11:36:04 +0000

    14/02/2023

--- lab2-BasicProgramStructure/tabs1.cpp
+++ lab2-BasicProgramStructure/tabs1.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "\t" << 1 << "\t" << 2 << "\n";
-    cout << "\t" << 3 << "\t" << 4 << "\n";
-    cout << "\t" << 5 << "\t" << 6 << "\n";
+    string name;
+    cout << "Please, enter your name:\n";
+    cin >> name;
+    cout << "\t" << name << "\t\n";
 
     return 0;
 }
