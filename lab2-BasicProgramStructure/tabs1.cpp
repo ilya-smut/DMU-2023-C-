023bda04356b9b82da49c482ac51aef45da5fe11
Ilya Smut <p2688910@dmu.ac.uk>
2023-02-14 11:27:34 +0000

    14/02/2023

---
+++ lab2-BasicProgramStructure/tabs1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\t" << 1 << "\t" << 2 << "\n";
+    cout << "\t" << 3 << "\t" << 4 << "\n";
+    cout << "\t" << 5 << "\t" << 6 << "\n";
+
+    return 0;
+}
