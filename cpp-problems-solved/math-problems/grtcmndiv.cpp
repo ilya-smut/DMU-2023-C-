d6cb088f2b8a104f326cb0c628867c036e753ec7
Ilya Smut <p2688910@dmu.ac.uk>
2023-02-21 12:23:19 +0000

    21/02/2023

---
+++ cpp-problems-solved/math-problems/grtcmndiv.cpp
@@ -0,0 +1,45 @@
+/*
+Greatest common divisor of two positive integers
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int cach;
+    int result;
+    bool equal = false;
+    cout << "Please, enter two positive integers: ";
+    cin >> num1 >> num2;
+
+    if (num1 < num2){
+        cach = num1;
+        num1 = num2;
+        num2 = cach;
+    }
+    else if (num2 == num1){
+        equal = true;
+    }
+
+    if (!equal) {
+
+        for (int i = num2; i > 0; i--){
+            if (num2 % i == 0 && num1 % i == 0){
+                result  = i;
+                break;
+            }
+        }
+
+    }
+    else
+    {
+        result = num2;
+    }
+
+    cout << "Greatest common divisor is " << result << endl;
+
+}
