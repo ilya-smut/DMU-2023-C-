90a9a24994da98586a49c7431b32fa3a1fc4a692
Ilya Smut <p2688910@my365.dmu.ac.uk>
2023-02-27 16:03:23 +0000

    27/02/2023

--- w3-lab1-functions/printRectangle.cpp
+++ w3-lab1-functions/printRectangle.cpp
@@ -12,11 +12,11 @@ void printRectangle(int height, int width, char fill){
 }
 
 int main(int argc, char* argv[]) {
-  int height = stoi(argv[1]);
-  int width = stoi(argv[2]);
-  char fill = argv[3][0];
-  if (fill < 97 || fill > 122){
-    fill = '*';
-  }
-  printRectangle(height, width, fill);
+    int height = stoi(argv[1]);
+    int width = stoi(argv[2]);
+    char fill = argv[3][0];
+    if (fill < 97 || fill > 122){
+        fill = '*';
+    }
+    printRectangle(height, width, fill);
 }
