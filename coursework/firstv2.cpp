98443ccb295fd4c6c87ee9ed158b9318ae0f9cff
Ilya Smut <p2688910@my365.dmu.ac.uk>
2023-04-07 19:17:48 +0100

    07.04.2023

--- coursework/firstv2.cpp
+++ coursework/firstv2.cpp
@@ -14,13 +14,16 @@ format. Make sure you check for division by zero error, other possible software
 errors and confirm the user's choice to exit the program.
 */
 
-#include <iostream>
-#include <string>
-#include <limits> 
+#include <iostream> 
+#include <string> // using strings to store the command entered by user
+#include <limits> // using limits to clear the cin buffer to avoid unpredicted behaviour
 
 using namespace std;
 
 void print_rules(){
+    /*
+    Function used to print the intstructions
+    */
     cout << endl << "-------Please-choose-the-command-from-the-following-list----------" << endl;
     cout << "sh • Convert seconds to hours (time must be entered in seconds) " << endl;
     cout << "sm • Convert seconds to minutes (time must be entered in seconds) " << endl;
@@ -33,65 +36,80 @@ void print_rules(){
 }
 
 void clear_buffer(){
+    // function used to clear the cin buffer to avoid unpredicted behaviour
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
 float seconds_to_hours(float seconds){
+    // each hour has 3600 seconds. We divide seconds by 3600
     return seconds/3600;
 }
 
 float seconds_to_minutes(float seconds){
+    // each minute has 60 seconds. We divide minutes by 60
     return seconds/60;
 }
 
 float minutes_to_hours(float minutes){
+    // each hour has 60 minutes. Same for seconds and minutes. We can use previous function
     return seconds_to_minutes(minutes);
 }
 
 float hours_to_seconds(float hours){
+    // each hour has 3600 seconds. We multiply hours by 3600 to get seconds
     return hours*3600;
 }
 
 float hours_to_minutes(float hours){
+    // each hour has 60 minutes. We multiply hours by 60 to get minutes
     return hours*60;
 }
 
 float minutes_to_seconds(float minutes){
+    // each minute has 60 seconds. Same for hours and minutes. We can use previous function
     return hours_to_minutes(minutes);
 }
 
 float get_input(){
-    float input;
-    while (true){
-        cin >> input;
-        if (cin.fail()){
-            cout << "Incorrect input. Please, enter the integer number" << endl;
+    // Function used to get input from the user
+
+    float input;                        // we store input as a float value to allow entering numbers such as 1.5 hours
+    while (true){                       // loop used to validate input
+        cin >> input;                   // getting input from user
+
+        if (cin.fail() || input < 0){   // if cin fails to get float value, or float value is less than 0, we clear buffer and ask user to enter value again
+            cout << "Incorrect input. Please, enter integer or float number greater than or equal to 0" << endl;
             clear_buffer();
         }
         else
         {
-            clear_buffer();
+            clear_buffer();             // if input is validated, we clear the buffer and break from the loop.
             break;
         }
     }
 
-    return input;
+    return input;   // After we get proper input (it is guaranteed since otherwise user cannot exit the loop) we return it (float number >= 0)
 }
 
 
 int main()
 {
-    bool play = true;
-    string command = "";
-    do{
-        print_rules();
-        getline(cin, command);
-        if (command == "sh")
+    bool play = true;                       // using play flag to control the flow of the game. Once it becomes false - game stops
+    string command = "";                    // using string variable to store the command entered by user
+    do{                                     // using do-while loop to check play == true after every execution of the loop.
+        print_rules();                      // printing instructions
+        getline(cin, command);              // getting command from the user
+
+        /*
+        Following if-else structure looks for specific commands in the user input and executes necessary option. But if the user input 
+        does not match any of the specified commands, user is asked to input the command again.
+        */
+        if (command == "sh")    
         {
             cout << "Converting seconds to hours. Please, enter the amount: " << endl;
             float input = get_input();
-            cout << input << " hours equal to " << seconds_to_hours(input) << " hours" << endl;
+            cout << input << " seconds equal to " << seconds_to_hours(input) << " hours" << endl;
         }
         else if(command == "sm")
         {
@@ -123,18 +141,37 @@ int main()
             float input = get_input();
             cout << input << " minutes equal to " << minutes_to_seconds(input) << " seconds" << endl;
         }
-        else if(command == "q")
+        else if(command == "q")             // if user types 'q' we need to confirm their intention to exit the program
         {
-            cout << "Thank you! See you next time!" << endl;
-            play = false;
+            while (true){                   // we confirm this intention by executing this loop
+
+                cout << "Are you sure that you want to exit? y/n?" << endl;     // we expect user to input 'y' or 'n'
+                char answer = ' ';  // we store input in this char variable
+                cin >> answer;
+
+                if (answer == 'y'){     // if user enters 'y', we change play to false and break the loop
+                    cout << "Thank you! See you next time!" << endl;
+                    play = false;
+                    break;
+                }
+                else if (answer == 'n'){    // if user enters 'n', we clear the buffer to avoid unexpected behaviour and break the loop.
+                    clear_buffer();
+                    break;                  // when the loop is broken, user is asked again to input the command
+                }
+                else{                // if input is neither 'y' nor 'n', then user is asked to input again. The corrrect format is once again explained
+                    cout << "Incorrect format. Please, enter 'y' for yes or 'n' for no" << endl;
+                    clear_buffer();
+                }
+            }
         }
         else
         {
-            cout << "Incorrect input. Please, enter the command using specified format." << endl;
+            /*
+                If the command does not match any of the specifeid in the instruction commands, user is asked to input the command again.
+            */
+            cout << "Incorrect input. Please, enter the command using specified format." << endl; 
         }
-        command = "";
+        command = "";   // after each iteration command is being cleared to avoid unexpected behaviour
     }
     while(play);
-
-    return 0;
 }
--- coursework/second.cpp
+++ coursework/second.cpp
@@ -7,7 +7,7 @@ password - Cyb3rTech!
 
 #include <iostream>
 #include <string>
-#include <cstdlib> // Required for rand() and srand()
+#include <cstdlib> // Required for rand() and srand(). It is used to get random computer's move
 #include <ctime>   // Required for time()
 #include <limits>  // Required for clearing cin buffer
 
@@ -18,12 +18,19 @@ class game{         // class which unites all methods necessary to play a rps ga
 
         bool is_authorised = false;     // flag that checks whether user is authorised to play a game. False by default
 
+
         bool authorisation(string login, string password){      // bool method to check credentials. Returns either true or false
             if (login == "guest" && password == "Cyb3rTech!") {
                 return true;
             } else {
                 return false;
                 }
+        }
+
+        void clear_buffer(){
+            // function used to clear the cin buffer to avoid unpredicted behaviour
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
 
         int statistics[2] = {0, 0};     // array used to store game statistics. index 0 = user's score, index 1 = computer's score
@@ -61,20 +68,26 @@ class game{         // class which unites all methods necessary to play a rps ga
         }
 
         int ask_for_input(){
-            char inp;
+            string inp;              // using this sting variable to store user input
+
+            /*
+            During tests I found out that if we use char inp and not string inp, then if we input "ssssssssss", char input will be equal to 's'
+            Even if we clear the cin buffer after receiving input, "sssssssss" will be taken as 's' anyway, and game will think that user chose scissors.
+            Therefore I decided to use string instead. Using string fixes this unpredicted behaviour
+            */
+
             bool correct = true;     // flag that indicates whether input format is correct or not. Correct by default
             do {
-                cin.clear();    // cleaning cin buffer
                 if (!correct){
                     cout << "Incorrect input" << endl;
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');    // using this piece of code to clean the buffer. It avoids bugs when user inputs many characters
+                    clear_buffer();
                 }
                 
                 cout << endl << "Rock - r, Paper - p, Scissors - s. To exit enter e" << endl;
                 cout << "Please, choose: ";
                 cin >> inp;
 
-                if (inp != 'r' && inp != 'p' && inp != 's' && inp != 'e')   // cheching whether input is in correct format
+                if (inp != "r" && inp != "p" && inp != "s" && inp != "e")   // cheching whether input is in correct format
                 {
                     correct = false;  
                 }
@@ -83,17 +96,17 @@ class game{         // class which unites all methods necessary to play a rps ga
             }
             while (!correct);
 
-            if (inp == 'r'){
+            if (inp == "r"){
                 cout << endl << "Thanks! You chose ROCK" << endl;
                 return 0;   // return index in 2d array that corresponds with user's choice
             }
                 
-            else if (inp == 'p')
+            else if (inp == "p")
             {
                 cout << endl << "Thanks! You chose PAPER" << endl;
                 return 1;   // return index in 2d array that corresponds with user's choice
             }
-            else if (inp == 's')
+            else if (inp == "s")
             {
                 cout << endl << "Thanks! You chose SCISSORS" << endl;
                 return 2;   // return index in 2d array that corresponds with user's choice
@@ -121,6 +134,8 @@ class game{         // class which unites all methods necessary to play a rps ga
                     cout << "Computer chose SCISSORS!" << endl;
                     return 2;
             }
+
+            return -1; // function returns -1 if there is unexpected behaviour and somehow switch statement is skipped
         }
 
         void assess_outcome(char outcome){      // this method prints outcome and adds points to necessary party. Takes input from get_outcome()
@@ -145,14 +160,15 @@ class game{         // class which unites all methods necessary to play a rps ga
             int attempt_limit = 3;  // user has 3 attempts to enter correct login and password
 
             while (is_authorised == false && attempt_limit > 0){    // user goes through the loop until he either logs in or uses all attempts
-                cout << "Please, enter your login: " << endl;
 
                 while (true){
+                    cout << "Please, enter your login: " << endl;
                     getline(cin, login);
                     if (login.size() < 11)      // size of the login must not be more than 10 characters. It helps to avoid bufferoverflow
                         break;
-                    else
-                        cout << "correct login format" << endl;
+                    else{
+                        cout << "Incorrect login format" << endl;
+                    }
                 }
 
                 cout << "Please, enter your password: " << endl;
@@ -161,14 +177,14 @@ class game{         // class which unites all methods necessary to play a rps ga
                 if (password.size() < 16){      // size of password must not be longer than 15 characters. It avoids bufferoverflow
                     is_authorised = authorisation(login, password);
                     if (!is_authorised){
-                        cout << "correct password" << endl;
+                        cout << "Incorrect password" << endl;
                         attempt_limit--;
                         cout << "You have " << attempt_limit << " more attempts" << endl << endl;
                         
                     }
                 }
                 else{
-                    cout << "correct password " << endl;
+                    cout << "Incorrect password " << endl;
                     attempt_limit--;
                     cout << "You have " << attempt_limit << " more attempts" << endl << endl;
                 }
@@ -176,6 +192,9 @@ class game{         // class which unites all methods necessary to play a rps ga
         }
 
         void play(){                        // the most important method of Game class. It makes rps game playable.
+
+            bool play = true; // using this boolean variable to check whether user wants to continue game or not. By default - true
+
             int game_counter = 1;           // game counter which will be used to count amount of played rounds
 
             if (!is_authorised){            // If user is not authorised - everything he sees is the following message and then game ends
@@ -184,18 +203,39 @@ class game{         // class which unites all methods necessary to play a rps ga
             else
                 print_rules();              // If user is authorised - he will be presented with rules on this step
 
-            while (is_authorised)           // If user is authorised - game starts
+            while (is_authorised && play)           // If user is authorised - game starts
             {
                 cout << "-------------- GAME " << game_counter << " ----------" << endl;
 
                 int players_move = ask_for_input(); // getting choice from player
 
-                if (players_move == 3){         // if player enters 'e' which returns exit code (3) - game stops
-                    this->print_statistics();   // print game statistics
-                    cout << "Thanks for playing! See you soon! :)" << endl;
-                    break;                      // breaking game cycle 
+                if (players_move == 3){         // if player enters 'e' which returns exit code (3) - they have intention to exit the game
+
+                    while (true){                   // we confirm this intention by executing this loop
+
+                        cout << "Are you sure that you want to exit? y/n?" << endl;     // we expect user to input 'y' or 'n'
+                        char answer = ' ';  // we store input in this char variable
+                        cin >> answer;
+
+                        if (answer == 'y'){     // if user enters 'y', we change play to false and break the loop
+                            this->print_statistics();   // print game statistics
+                            cout << "Thanks for playing! See you soon! :)" << endl;
+                            play = false;               // turn game flag to false. It will stop game.
+                            break;                      // break out of validation loop
+                        }
+                        else if (answer == 'n'){    // if user enters 'n', we clear the buffer to avoid unexpected behaviour and break the loop.
+                            clear_buffer();
+                            break;                  // when the loop is broken, user is asked again to input the command
+                        }
+                        else{                // if input is neither 'y' nor 'n', then user is asked to input again. The corrrect format is once again explained
+                            cout << "Incorrect format. Please, enter 'y' for yes or 'n' for no" << endl;
+                            clear_buffer();
+                        }
+                    }
+                    continue;
                 }
 
+                
                 int ai_move = get_ai_input();   // getting random move from computer
 
                 char outcome = get_outcome(players_move, ai_move);      // getting an outcome of the game
